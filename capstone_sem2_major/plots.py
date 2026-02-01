ca6190784fe4f3ac5eeb0a85b13e01ce8369beef
Emily Donofrio <donofrem@bc.edu>
2026-02-01 17:12:48 -0500

    create neighborhood area chart

--- capstone_sem2_major/plots.py
+++ capstone_sem2_major/plots.py
@@ -5,6 +5,8 @@ from tqdm import tqdm
 import typer
 
 from capstone_sem2_major.config import FIGURES_DIR, PROCESSED_DATA_DIR
+import pandas as pd
+import matplotlib.pyplot as plt
 
 app = typer.Typer()
 
@@ -13,14 +15,22 @@ app = typer.Typer()
 def main(
     # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
     input_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
-    output_path: Path = FIGURES_DIR / "plot.png",
+    output_path: Path = FIGURES_DIR / "arealand_sqmi_by_neighborhood.png",
     # -----------------------------------------
 ):
     # ---- REPLACE THIS WITH YOUR OWN CODE ----
     logger.info("Generating plot from data...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
+    
+    # make bar chart of arealand_sqmi by neighborhood
+    df = pd.read_csv(input_path)
+    plt.figure(figsize=(10, 6))
+    plt.bar(df["neighborhood"], df["arealand_sqmi"])
+    plt.xlabel("Neighborhood")
+    plt.ylabel("Area Land (sq mi)")
+    plt.title("Area Land by Neighborhood")
+    plt.xticks(rotation=45, ha="right")
+    plt.tight_layout()
+    plt.savefig(output_path)
     logger.success("Plot generation complete.")
     # -----------------------------------------
 
